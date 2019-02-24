d82fce7e62f103e16a9eb80ac59b65526c08b8b4
NortonAntiviral <officialantiviral@gmail.com>
2019-02-24 10:10:34 -0500

    added new pages

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -2,10 +2,14 @@
 
 class ProductController {
     index({view}){
-        return view.render('products/all');
+        const products = [1,2,3,4,5,6,7,8,9,10];
+        return view.render('products/all', {products});
+    }
+    newArrivals({view}){
+        const products = [1,2,3,4,5,6,7,8,9,10];
+        return view.render('products/all', {products});
     }
 }
-
 
 
 module.exports = ProductController
--- start/routes.js
+++ start/routes.js
@@ -17,7 +17,12 @@
 const Route = use('Route')
 
 Route.get('/', 'PageController.home');
-
 Route.get('/about', 'PageController.about');
 
-Route.get('/products', "ProductController.index")
+Route.get('/products', 'ProductController.index');
+Route.get('/products/new-arrivals', 'ProductController.newArrivals');
+
+//User Authentication
+Route.get('/register', 'UserController.register');
+Route.get('/login', 'UserController.login');
+
