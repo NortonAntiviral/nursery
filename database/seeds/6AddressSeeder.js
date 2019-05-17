1ba06d3c54db0c1e568a9542729ab72aea2dcd37
NortonAntiviral <officialantiviral@gmail.com>
2019-05-17 11:46:57 -0400

    added adress 2 and state

--- database/seeds/6AddressSeeder.js
+++ database/seeds/6AddressSeeder.js
@@ -18,12 +18,53 @@ class AddressSeeder {
   async run () {
     try {
       const customer = await Database.raw(`
-      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode,user_id)
-      Values("2121 Elm Street", "apt 21", "miami", )
+      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode, address_type, user_id)
+      Values("2121 Elm Street", "apt 21", "miami","fl", "USA", "34567", "shipping", 1 )
       `)
     } catch (error) {
       console.log(error);
     }
+    try {
+      const customer = await Database.raw(`
+      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode, address_type, user_id)
+      Values("4175 Crenshaw Blvd", "apt 2", "oakland","ca", "USA", "41754", "billing", 1 )
+      `)
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const customer = await Database.raw(`
+      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode, address_type, user_id)
+      Values("2941 Cambridge Dr", "apt 302", "richmond","va", "USA", "63205", "shipping", 2 )
+      `)
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const customer = await Database.raw(`
+      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode, address_type, user_id)
+      Values("2941 Cambridge Dr", "apt 302", "richmond","va", "USA", "63205", "billing", 2 )
+      `)
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const customer = await Database.raw(`
+      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode, address_type, user_id)
+      Values("328 grand ave", "apt 4g", "brooklyn","ny", "USA", "11745", "shipping", 3 )
+      `)
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const customer = await Database.raw(`
+      INSERT INTO nursery.addresses(address,address_2, city, state, country, zipcode, address_type, user_id)
+      Values("358 billy st", "", "flushing","ny", "USA", "11358", "billing", 3 )
+      `)
+    } catch (error) {
+      console.log(error);
+    }
+    console.log('Added addresses to customers')
   }
 }
 
